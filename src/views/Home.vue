d860afb1dda9ddb89f0bb8218dd50a0b19328944
OlehSirbu <oleh@bookinglayer.com>
2024-06-12 11:43:30 +0200

    chore: Refactor Home.vue component
    The code changes refactor the Home.vue component by using the Composition API from Vue 3. This improves code organization and readability by separating the component's logic into the `setup` function.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,36 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    <div>name:{{ name }} - age: {{ age }}</div>
+    <button @click="change">Change</button>
   </div>
 </template>
 
 <script>
+import { ref } from "vue";
 export default {
   name: "HomeComponent",
+  setup() {
+    const name = ref("Mario");
+    const age = ref(35);
+
+    const change = () => {
+      name.value = "Luigi";
+      age.value = 40;
+    };
+
+    return {
+      name,
+      age,
+      change,
+    };
+  },
+  created() {
+    console.log("created :>>");
+  },
+  mounted() {
+    console.log("mounted :>>");
+  },
   data() {
     return {};
   },
